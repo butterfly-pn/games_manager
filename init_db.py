20c189dcb7be934c44b3ffe20deb162f4c1d1950
Wikwoj0512 <wiktor.wojtkowiak@gmail.com>
2018-03-25 23:25:59 +0200

    reset bazy danych bez utraty informacjio użytkownikach

--- init_db.py
+++ init_db.py
@@ -13,49 +13,47 @@ def db_start():
     db.create_all()
     db.session.commit()
 
-    user = models.User()
-    user.username = "piotr"
-    user.password = sha256_crypt.encrypt("pppp1234")
-    user.email = 'piotr@dyba.com.pl'
-    user.job= "szef projektu"
-    user.admin = True
-    user.poweruser = True
-    user.organizer = True
-    user.birthdate = "01.01.1999"
-    user.about = "Jestem dobrym adminem"
-    user.why = "Chcę organizować gamejam, poniewż uważam, że jest to jest to idealna forma rozwoju dla młodych ludzi."
-    user.a=True
-    db.session.add(user)
-    db.session.commit()
-
-    team = models.Team()
-    team.name = "BestTeam"
-    team.master = "piotr"
-    team.master_email = 'piotr@dyba.com.pl'
-    team.contributors = ['adam', 'twojamama', 'jakiskoles']
-    db.session.add(team)
-    db.session.commit()
-
-    message=models.Message()
-    message.title = 'testowa wiadomość'
-    message.adresser='piotr'
-    message.author='wikwoj'
-    message.content='No hej'
-    message.created=datetime.now()
-    db.session.add(message)
-    db.session.commit()
-
-
-
-
-    jam = models.Jam()
-    jam.title = "Jam testowy"
-    jam.master="piotr"
-    jam.description = "testowy jam"
-    jam.master_email = 'piotr@dyba.com.pl'
-    jam.teams=['BestTeam']
-    db.session.add(jam)
-    db.session.commit()
+    # user = models.User()
+    # user.username = "piotr"
+    # user.password = sha256_crypt.encrypt("pppp1234")
+    # user.email = 'piotr@dyba.com.pl'
+    # user.job= "szef projektu"
+    # user.admin = True
+    # user.poweruser = True
+    # user.organizer = True
+    # user.birthdate = "01.01.1999"
+    # user.about = "Jestem dobrym adminem"
+    # user.why = "Chcę organizować gamejam, poniewż uważam, że jest to jest to idealna forma rozwoju dla młodych ludzi."
+    # user.a=True
+    # db.session.add(user)
+    # db.session.commit()
+    #
+    # team = models.Team()
+    # team.name = "BestTeam"
+    # team.master = "piotr"
+    # team.master_email = 'piotr@dyba.com.pl'
+    # team.contributors = ['adam', 'twojamama', 'jakiskoles']
+    # db.session.add(team)
+    # db.session.commit()
+    #
+    # message=models.Message()
+    # message.title = 'testowa wiadomość'
+    # message.adresser='piotr'
+    # message.author='wikwoj'
+    # message.content='No hej'
+    # message.created=datetime.now()
+    # db.session.add(message)
+    # db.session.commit()
+    #
+    #
+    # jam = models.Jam()
+    # jam.title = "Jam testowy"
+    # jam.master="piotr"
+    # jam.description = "testowy jam"
+    # jam.master_email = 'piotr@dyba.com.pl'
+    # jam.teams=['BestTeam']
+    # db.session.add(jam)
+    # db.session.commit()
 
 if __name__ == '__main__':
     db_start()
---
+++ reset_main.py
@@ -0,0 +1,29 @@
+__author__ = 'Wiktor Wojtkowiak & Sebastian Cichy & Patryk Niedźwiedziński'
+
+from os import path
+from config import Config
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reset.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy()
+db.app = app
+db.init_app(app)
+lm = LoginManager()
+lm.init_app(app)
+bcrypt = Bcrypt()
+CONFIG = Config()
+
+app.static_path = path.join(path.abspath(__file__), 'static')
+
+
+
+if __name__ == '__main__':
+    from views import *
+    app.run(debug=CONFIG.DEBUG, host=CONFIG.HOST, port=CONFIG.PORT)
+
